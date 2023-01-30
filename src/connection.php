21a3b3379cd3457ef8d0961cbd0034e4f11979a5
Vinicius Martins <74664750+LeVixen@users.noreply.github.com>
2023-01-30 13:56:49 -0300

    env: creat a connection in php

---
+++ src/connection.php
@@ -0,0 +1,9 @@
+<?php
+   $host = "localhost";
+   $user = "root";
+   $passwd = "";
+   $db =  "login";
+
+   $conection = mysqli_connect($host, $user, $passwd, $db) or die ("Não foi possível conectar ao bando de dados"); 
+?>
+
